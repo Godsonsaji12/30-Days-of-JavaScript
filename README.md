45aa8880876acefe4a33cc1c1d0cfaf8df756337
Godsonsaji12 <Godsonsaji077@gmail.com>
2024-07-13 20:27:19 +0530

    Added Files

---
+++ README.md
@@ -0,0 +1,2 @@
+# 30 Days of JavaScript
+ This is the Reopsitry that contains all the solution of assignments
---
+++ day1.js
@@ -0,0 +1,49 @@
+// ACTIVITY-1
+
+//Task-1
+var num= 10;
+console.log(num);
+
+//Task-2
+let fullName = "Godson Saji";
+console.log(fullName);
+
+// ACTIVITY-2
+
+//Task-3
+const loggedIn = true;
+console.log(loggedIn);
+
+// ACTIVITY-3
+
+//Task-4
+let objOfUser = {
+    name:"Godson Saji",
+    age:19,
+    isStudent:true,
+};
+console.log(typeof objOfUser.name);
+console.log(typeof objOfUser.age);
+console.log(typeof objOfUser.isStudent);
+
+let arrOfHobbies=["Playing Games","Coding","Reading Books"];
+console.log(typeof arrOfHobbies);
+
+// ACTIVITY-4
+//Task-5
+
+let initialValue = 10;
+console.log(initialValue);
+initialValue = 20;
+console.log(initialValue);
+
+// ACTIVITY-5
+//Task-6
+
+const initialValue2 = 30;
+console.log(initialValue2);
+try {
+    initialValue2 = 40
+} catch (error) {
+    console.log("There was an error!")
+}
